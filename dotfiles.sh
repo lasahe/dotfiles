af3f83cc43ddd69003a8944e60d4d7b40fae6d80
lasahe <lasahe@gmail.com>
2023-01-19 21:11:31 +0200

    Create dotfiles.sh

---
+++ dotfiles.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Define colors...
+RED=`tput bold && tput setaf 1`
+GREEN=`tput bold && tput setaf 2`
+YELLOW=`tput bold && tput setaf 3`
+BLUE=`tput bold && tput setaf 4`
+NC=`tput sgr0`
+
+function RED(){
+   echo -e "\n${RED}${1}${NC}"
+}
+function GREEN(){
+   echo -e "\n${GREEN}${1}${NC}"
+}
+function YELLOW(){
+   echo -e "\n${YELLOW}${1}${NC}"
+}
+function BLUE(){
+   echo -e "\n${BLUE}${1}${NC}"
+}
+
+# Testing if root...
+if [ $UID -ne 0 ]
+then
+   RED "You must run this script as root!" && echo
+   exit
+fi
+
+# Get .tmux conf
+BLUE "Installing Tmux,vim..."
+sudo apt install -y tmux vim
+
+## Install tmux config
+wget https://raw.githubusercontent.com/lasahe/dotfiles/main/.tmux.conf -O ~/.tmux.conf
+
+BLUE "Installing Go..."
+sudo apt install -y golang-go
+BLUE "Adding GOPATH and GOBIN to .bashrc, so future installs are easy.."
+grep "export GOPATH" ~/.bashrc
+if [ $? -eq 1 ]
+then
+   echo "export GOPATH=\$HOME/.go/" >> ~/.bashrc
+fi
+grep "export GOBIN" ~/.bashrc
+if [ $? -eq 1 ]
+then
+   echo "export GOBIN=\$HOME/.go/bin" >> ~/.bashrc
+   echo "export PATH=\$PATH:\$GOBIN" >> ~/.bashrc
+fi
+
+BLUE "Setting Keyboard to FI..."
+setxkbmap -layout fi 
+
+BLUE "Installing net-tools,ssh,tcpdump,curl..."
+sudo apt install -y net-tools ssh curl tcpdump mtr-tiny
+
+BLUE "Installing terminator..."
+sudo apt install -y terminator
+
+# Change terminator as default terminal
+sudo update-alternatives --config x-terminal-emulator
